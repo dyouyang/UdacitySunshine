b7e947c6baffaa8ae5b567b36afcf361978a31b8
David Ouyang <douyang@blizzard.com>
2015-03-20 09:21:04 -0700

    Add new asynctask to fetch weather data.
    We also moved the main fragment to its own file for cleanliness.  The
    task to fetch weather data is not currently being used.

--- app/src/main/java/com/douyang/sunshine/MainActivity.java
+++ app/src/main/java/com/douyang/sunshine/MainActivity.java
@@ -1,18 +1,23 @@
 package com.douyang.sunshine;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 
@@ -24,7 +29,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -52,37 +57,5 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            // Generate some static list data.
-            ArrayList<String> forecasts = new ArrayList<>();
-            forecasts.add("Today - sunny");
-            forecasts.add("Tomorrow - cloudy");
-            forecasts.add("Day 3 - hail");
-
-            // The adapter responsible for generating each list item.
-            ArrayAdapter<String> forecastsAdapter = new ArrayAdapter<String>(
-                    getActivity(),
-                    R.layout.list_item_forecast,
-                    R.id.list_item_forecast_textview,
-                    forecasts);
-
-            // Assign the adapter to the list.
-            ListView forecastsList = (ListView) rootView.findViewById(R.id.listview_forecast);
-            forecastsList.setAdapter(forecastsAdapter);
-
-            return rootView;
-        }
-    }
 }
